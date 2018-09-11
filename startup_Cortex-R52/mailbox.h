8572700ce965878d75bd7aaa79208a2ebfa23405
Alexei Colin <acolin@isi.edu>
2018-09-11 10:39:18 -0400

    mailbox: fix base address to match HW

--- startup_Cortex-R52/mailbox.h
+++ startup_Cortex-R52/mailbox.h
@@ -3,8 +3,8 @@
 
 #include <stdint.h>
 
-#define HPPS_RTPS_MBOX_BASE 	((volatile uint32_t *)0xf9220000)
-#define HPPS_TRCH_MBOX_BASE 	((volatile uint32_t *)0xf9230000)
+#define HPPS_RTPS_MBOX_BASE 	((volatile uint32_t *)0xf9230000)
+#define HPPS_TRCH_MBOX_BASE 	((volatile uint32_t *)0xf9220000)
 #define RTPS_TRCH_MBOX_BASE	((volatile uint32_t *)0x3000a000)
 
 #define RTPS_TRCH_MBOX_HAVE_DATA_IRQ    162
